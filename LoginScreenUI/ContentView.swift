2062c44b4cca7da961a59405a9ecda1e71962330
Jevone Barrett <jevonebarrett@gmail.com>
2022-06-27 21:31:51 -0400

    Initial Commit

---
+++ LoginScreenUI/ContentView.swift
@@ -0,0 +1,87 @@
+//
+//  ContentView.swift
+//  LoginScreenUI
+//
+//  Created by Jevone Barrett on 6/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    
+    
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.green
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50 )
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50 )
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                      authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.red)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+            }
+            
+        }
+            .navigationBarHidden(true)
+    }
+}
+    
+    func authenticateUser(username: String, password: String) {if username.lowercased() == "jevone18"{
+        wrongUsername = 0
+        if password.lowercased() == "abc123"{
+            wrongPassword = 0
+            showingLoginScreen = true
+        } else {
+            wrongPassword = 2
+        }
+    } else{
+        wrongPassword = 2
+    }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Jevone Barrett on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
